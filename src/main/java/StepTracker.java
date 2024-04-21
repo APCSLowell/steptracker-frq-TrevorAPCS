e126ab4a89f65d9e00044c0c99c4bb38ef983983
TrevorAPCS <142834649+TrevorAPCS@users.noreply.github.com>
2024-04-21 09:06:47 -0700

    Update StepTracker.java

--- src/main/java/StepTracker.java
+++ src/main/java/StepTracker.java
@@ -1,5 +1,21 @@
 import java.util.ArrayList;
 public class StepTracker
 {
- /* to be implemented here */
+ private int minSteps, activeDays, totalSteps, totalDays;
+	public StepTracker(int mSteps){
+		minSteps = mSteps;
+	}
+	public void addDailySteps(int steps){
+		totalSteps += steps;
+		totalDays++;
+		if(steps >= minSteps){
+			activeDays++;
+		}
+	}
+	public int activeDays(){
+		return activeDays;
+	}
+	public int averageSteps(){
+		return totalSteps / totalDays;
+	}
 } 
